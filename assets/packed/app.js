3085b14948b204fb613c625cb544b6d4a4a25e53
root <root@localhost.localdomain>
2020-08-27 22:31:55 +0800

    20200827

--- assets/packed/app.js
+++ assets/packed/app.js
@@ -6329,7 +6329,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36372" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36066" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
